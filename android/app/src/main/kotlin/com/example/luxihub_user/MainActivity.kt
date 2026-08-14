59b140e3a35f9e33c0dbbf24c44fbaed22eecd03
Mahamudul.Dev <mahamudul.dev@gmail.com>
2026-08-14 22:03:11 +0600

    fix: Change MainActivity to extend FlutterFragmentActivity instead of FlutterActivity

--- android/app/src/main/kotlin/com/example/luxihub_user/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.luxihub_user
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity()
--- android/app/src/main/kotlin/com/luxihub/user_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.luxihub.user_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
